8e58de8223a7f8346efd7e29783ade1875e5c5b0
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2020-08-06 18:27:25 +0200

    merging matched it skills and unmatched it skills

--- merge_skill_files.py
+++ merge_skill_files.py
@@ -4,7 +4,21 @@ import os
 import argparse
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--matched', type=str, required=True)
+    parser.add_argument('--unmatched', type=str, required=True)
+    parser.add_argument('--output_dir', type=str, required=True)
+    args = parser.parse_args()
+
+    matched_df = pd.read_csv(args.matched)
+    unmatched_df = pd.read_csv(args.unmatched)
+
+    it_skills = pd.concat([matched_df, unmatched_df.loc[unmatched_df['is_it'] == 1, ['Skill']]])
+    non_it_skills = unmatched_df.loc[unmatched_df['is_it'] == 2]
+
+    it_skills.to_csv(os.path.join(args.output_dir, 'it_skills.csv'), index=False)
+    non_it_skills.to_csv(os.path.join(args.output_dir, 'other_skills.csv'), index=False)
+
 
 if __name__ == '__main__':
     main()
