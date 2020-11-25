fix for normalisation issue

utilities/analysis_utils.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.decomposition import PCA, TruncatedSVD
 from utilities.pandas_utils import *
 from utilities.constants import *
 from utilities.params import *
@@ -70,18 +69,19 @@ def fill_in_the_blank_dates(df, method='zero', has_company=True):
 def group_time_steps_together(df, steps_to_group=3, has_company=True):
     if steps_to_group == 1:
         return df
+    dates_list = [pd.to_datetime(x) for x in df[['Date']].drop_duplicates().sort_values('Date').Date.values]
+    dates_dict = {dates_list[i]: 1 + (i // steps_to_group) for i in range(len(dates_list))}
+    min_dates = {1 + (i // steps_to_group): dates_list[i] for i in range(len(dates_list)) if i % steps_to_group == 0}
+    result_df = df.copy()
+    result_df['Date'] = result_df.Date.apply(lambda x: min_dates[dates_dict[x]])
     if has_company:
         base_cols = ['Date', 'Company']
+        totals_fixed = result_df[base_cols + ['Total']].drop_duplicates().groupby(base_cols).sum().reset_index()
+        result_df = result_df[base_cols+['Skill', 'Job Postings Raw']].groupby(base_cols+['Skill']).sum().reset_index()
+        result_df = pd.merge(result_df, totals_fixed, on=base_cols)
     else:
         base_cols = ['Date']
-    dates_df = df[base_cols].drop_duplicates().sort_values(base_cols).reset_index().drop(columns=['index'])
-    dates_df['group'] = pd.Series(list(range(dates_df.shape[0])))
-    dates_df['group'] = dates_df['group'].apply(lambda x: 1 + (x // steps_to_group))
-    print(dates_df)
-    min_dates = dates_df.groupby('group').min().reset_index()
-    result_df = pd.merge(df, dates_df, on=base_cols).drop(columns=['Date']).groupby(['group', 'Skill']).mean(). \
-        reset_index()
-    result_df = pd.merge(result_df, min_dates, on='group').drop(columns=['group'])
+        result_df = result_df.groupby(base_cols).sum().reset_index()
     return result_df
 
 def smooth_and_normalise_timeseries(df, log, normaliser=None, smooth=None, y_col='Job Postings',
@@ -387,10 +387,13 @@ def multiple_skill_pop_profiles(df, skills, pop_type, time_period=None, normalis
 def clean_nan_features(df_dict, colname='tsfresh', feature_names=None):
     dict_keys = list(df_dict.keys())
     features_matrix = np.vstack([series_to_matrix(df_dict[k][colname]) for k in dict_keys])
-    non_nan_cols = (~np.any(np.isnan(features_matrix), axis=0)) & (~np.any(np.isinf(features_matrix), axis=0))
-    features_matrix = features_matrix[:, non_nan_cols]
+    non_nan_and_nonzero_variance_cols = \
+        (~np.any(np.isnan(features_matrix), axis=0)) & (~np.any(np.isinf(features_matrix), axis=0) &
+                                                        (np.std(features_matrix, axis=0) > 0.0))
+
+    features_matrix = features_matrix[:, non_nan_and_nonzero_variance_cols]
     if feature_names is not None:
-        feature_names = [feature_names[i] for i in range(len(feature_names)) if non_nan_cols[i] == True]
+        feature_names = [feature_names[i] for i in range(len(feature_names)) if non_nan_and_nonzero_variance_cols[i] == True]
     # if n_features is not None:
     #     reduction_model = TruncatedSVD(n_components=n_features)
     #     features_matrix = reduction_model.fit_transform(features_matrix)
@@ -405,13 +408,21 @@ def clean_nan_features(df_dict, colname='tsfresh', feature_names=None):
     return df_dict, feature_names
 
 
-def compute_total_poptype_mean(df, pop_type='log'):
+def compute_total_poptype_mean(df, pop_type='log', based_on='skill'):
     """
     For each date, computes the average, among all companies, of the desired poptype of their total number of ads.
     """
-    return df[['Date', 'Company', 'Total']].drop_duplicates().drop(columns=['Company'])\
-            .groupby('Date').apply(lambda x: np.mean(x['Total'].apply(lambda y: pop_type_function(y, pop_type)))).\
-                                                            reset_index().rename(columns={0: 'Total'})
+    if based_on == 'company':
+        return df[['Date', 'Company', 'Total']].drop_duplicates().drop(columns=['Company'])\
+                .groupby('Date').apply(lambda x: np.mean(x['Total'].apply(lambda y: pop_type_function(y, pop_type)))
+                                                    ).\
+                                                                reset_index().rename(columns={0: 'Total'})
+    else:
+        return df[['Date', 'Skill', 'Job Postings Raw']].groupby(['Date', 'Skill']).\
+            apply(lambda x: np.sum(x['Job Postings Raw'].apply(lambda y: pop_type_function(y, pop_type)))).\
+                reset_index().rename(columns={0: 'Total'}).drop(columns=['Skill'])\
+                    .groupby('Date').median().\
+                        reset_index().rename(columns={0: 'Total'})
 
 def compute_total_values(df):
     return df[['Date', 'Company', 'Total']].drop_duplicates().groupby('Date').\

utilities/classifier_utils.py
@@ -6,6 +6,8 @@ from sklearn.metrics import precision_recall_fscore_support, f1_score, confusion
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC, LinearSVC
+from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.base import clone as sklearn_clone
@@ -120,8 +122,8 @@ def create_train_test_split(x_datapoints, y_datapoints, test_proportion=0.2, cla
 
     return sampled_train_x_df, sampled_test_x_df, sampled_train_y_df, sampled_test_y_df
 
-def pre_normalise_and_pca(sampled_train_x_df, sampled_test_x_df, n_features=50,
-                          pre_normaliser=None, post_normaliser=None, feature_col='tsfresh'):
+def pre_normalise_and_pca(sampled_train_x_df, sampled_test_x_df, sampled_train_y_df, sampled_test_y_df, n_features=50,
+                          pre_normaliser=None, post_normaliser=None, feature_col='tsfresh', do_pca=True):
 
     train_features = series_to_matrix(sampled_train_x_df[feature_col])
     test_features = series_to_matrix(sampled_test_x_df[feature_col])
@@ -129,11 +131,19 @@ def pre_normalise_and_pca(sampled_train_x_df, sampled_test_x_df, n_features=50,
         train_features = pre_normaliser.fit_transform(train_features)
         test_features = pre_normaliser.transform(test_features)
     if n_features is not None:
-        pca_model = PCA(n_components=n_features)
-        train_features = pca_model.fit_transform(train_features)
-        test_features = pca_model.transform(test_features)
+        if do_pca:
+            feature_reduction_model = PCA(n_components=n_features)
+            train_features = feature_reduction_model.fit_transform(train_features)
+            test_features = feature_reduction_model.transform(test_features)
+        else:
+            feature_reduction_model = SelectKBest(score_func=mutual_info_classif, k=n_features)
+            train_features = feature_reduction_model.fit_transform(train_features,
+                               sampled_train_y_df.loc[sampled_train_y_df.common_index.
+                                       apply(lambda x: x in sampled_train_x_df.common_index.values)].\
+                                                                   row_class.values)
+            test_features = feature_reduction_model.transform(test_features)
     else:
-        pca_model = None
+        feature_reduction_model = None
     if post_normaliser is not None:
         if post_normaliser == 'unit':
             train_features = train_features / np.linalg.norm(train_features, axis=1).\
@@ -148,7 +158,7 @@ def pre_normalise_and_pca(sampled_train_x_df, sampled_test_x_df, n_features=50,
     sampled_test_x_df[feature_col + '_orig'] = sampled_test_x_df[feature_col]
     sampled_train_x_df[feature_col] = train_features.tolist()
     sampled_test_x_df[feature_col] = test_features.tolist()
-    return sampled_train_x_df, sampled_test_x_df, pca_model, pre_normaliser, post_normaliser
+    return sampled_train_x_df, sampled_test_x_df, feature_reduction_model, pre_normaliser, post_normaliser
 
 def filter_out_negatives(x_datapoints, rawpop_upper_bounds=None, y_datapoints=None, pop_col=POP_COL,
                          return_fns=False):
@@ -285,7 +295,7 @@ def evaluate_results(y_pred, y_truth, type='acc'):
 
 def predict_and_evaluate_dfs(clf_model, x_datapoints, y_datapoints,
                              rawpop_upper_bounds=None, normaliser=None, eval_type='f1', features_col=FEATURE_COL,
-                             return_modified_df=False):
+                             return_modified_df=False, aggregate_for_skills=False):
     """
     Wrapper for predicting and evaluating the predictions.
     :param clf_model: Classifier model
@@ -297,14 +307,26 @@ def predict_and_evaluate_dfs(clf_model, x_datapoints, y_datapoints,
     """
     y_pred = predict_results_df(clf_model, x_datapoints, y_datapoints.common_index.values, rawpop_upper_bounds,
                                 normaliser=normaliser, features_col=features_col)
-    evaluation = evaluate_results(y_pred, y_datapoints[TRUTH_COL].values, eval_type)
-    if not return_modified_df:
-        return y_pred, evaluation
-    else:
-        return y_pred, evaluation, \
-                    y_datapoints.copy().assign(pred=y_pred).loc[y_pred == 1], \
-                    y_datapoints.copy().assign(pred=y_pred).loc[y_datapoints.row_class == 1],\
-                    y_datapoints.copy().assign(pred=y_pred)
+    if not aggregate_for_skills:
+        evaluation = evaluate_results(y_pred, y_datapoints[TRUTH_COL].values, eval_type)
+        if not return_modified_df:
+            return y_pred, evaluation
+        else:
+            return y_pred, evaluation, \
+                        y_datapoints.assign(pred=y_pred).loc[y_pred == 1], \
+                        y_datapoints.assign(pred=y_pred).loc[y_datapoints.row_class == 1],\
+                        y_datapoints.assign(pred=y_pred)
+    if aggregate_for_skills:
+        result_df = y_datapoints.assign(pred=y_pred)
+        result_df = result_df[['Skills', 'row_class', 'pred']].groupby('Skills').sum().reset_index()
+        result_df['row_class'] = result_df['row_class'].apply(lambda x: 1 if x > 0 else 0)
+        result_df['pred'] = result_df['pred'].apply(lambda x: 1 if x > 0 else 0)
+        evaluation = evaluate_results(result_df.pred.values, result_df.row_class.values, eval_type)
+        return result_df.pred, evaluation, \
+               result_df.loc[result_df.pred == 1], \
+               result_df.loc[result_df.row_class == 1],\
+               result_df
+
 
 def generate_cv_folds(x_train_df, y_train_df, cv_folds=CV_FOLDS, stratified=True, random_state=1):
     """
@@ -505,7 +527,7 @@ def cross_validate_with_quantile(cv_data, period_to_df, normaliser=None, verbose
     best_c = cs[np.argmax(scores)]
     best_q = qs[np.argmax(scores)]
 
-    return best_model, best_c, best_q, best_score
+    return best_model, best_c, best_q, best_score, rawpop_upper_bound
 
 
 